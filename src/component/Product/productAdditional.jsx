b5a2aab3c50769b145cd93410c1a0bfb71aa1902
Amvionlab <webdeveloper2@gmail.com>
2025-12-29 09:47:29 +0530

    added points

---
+++ src/component/Product/productAdditional.jsx
@@ -0,0 +1,65 @@
+const productAdditionalData = {
+    "biochemistry-analyzer": [
+        {
+            level: "h2",
+            title: "What Is a Biochemistry Analyzer?",
+            content: [
+                "A biochemistry analyzer is a clinical chemistry analyzer- a diagnostic instrument used to measure the chemical and biochemical components present in blood and other body fluids. These systems play a crucial role in evaluating liver function, kidney performance, lipid profiles, electrolytes, hormones, and metabolic markers.",
+                "Modern labs depend on analyzers to deliver accurate, fast, and high-throughput chemistry testing, supporting everything from routine health checks to complex disease diagnosis. Depending on the workload, laboratories choose between fully automatic biochemistry analyzers for complete automation or semi automatic biochemistry analyzers for flexible, cost-efficient testing. Both types use specialized biochemistry reagents and reagent kits to generate precise and reproducible results.",
+                "A biochemistry machine  is considered the backbone of chemical pathology and clinical diagnostics—making it an essential tool for hospitals, diagnostic centers, and small to mid-sized laboratories."
+            ]
+        },
+        {
+            level: "h3",
+            title: "Role of Biochemistry Machines in Modern Diagnostics",
+            content:
+                "Biochemistry machines help laboratories deliver fast and reliable chemical analysis for patient diagnosis. They support routine and advanced testing required for disease detection, treatment monitoring, and preventive healthcare."
+        },
+        {
+            level: "h3",
+            title: "How Clinical Chemistry Analyzers Improve Testing Accuracy",
+            content:
+                " It improves accuracy by automating critical steps such as reagent handling, incubation, and measurement. This reduces manual variability and ensures consistent, reproducible test results."
+        },
+
+        {
+            level: "h2",
+            title: "Types of Biochemistry Analyzers",
+            content:
+                "Biochemistry analyzers come in different formats to suit the testing volume and workflow of a clinical lab:"
+        },
+
+        {
+            level: "h3",
+            title: "Fully Automatic Biochemistry Analyzer",
+            content:
+                "A fully automated biochemistry analyser automates every step of testing—sample loading, reagent dispensing, mixing, incubation, photometric measurement, and reporting.Ideal for:",
+            list: [
+                "High-throughput labs",
+                "24/7 diagnostic centers",
+                "Labs requiring consistent quality and rapid turnaround time",
+                "Minimal manual intervention",
+                "Automated QC and calibration",
+                "Onboard reagent storage",
+                "Continuous sample loading",
+            ],
+            cta: {
+                text: "Explore Products",
+                url: "https://cpcdiagnostics.in/biochemistry-analyzer/fully-automatic/"
+            }
+        },
+
+        // {
+        //     level: "h4",
+        //     title: "Key Features",
+        //     list: [
+        //         "Automated QC",
+        //         "High throughput testing",
+        //         "Onboard reagent storage"
+        //     ]
+        // }
+    ],
+
+};
+
+export default productAdditionalData;
